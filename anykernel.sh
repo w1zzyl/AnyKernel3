c83edbb0c0027aaeb821d93407f285c1da6e17fa
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-11-15 22:05:09 +0000

    AK3: Rebrand and adapt for alioth

--- anykernel.sh
+++ anykernel.sh
@@ -4,26 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Viper by w1zzyl
+kernel.string=Xraz Kernel by w1zzyl & xrycenx
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vayu
-device.name2=
+device.name1=alioth
+device.name2=aliothin
 device.name3=
 device.name4=
 device.name5=
 supported.versions=
-supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -33,29 +31,19 @@ patch_vbmeta_flag=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 
 
-## AnyKernel boot install
+## AnyKernel install
 dump_boot;
+
+# Begin Ramdisk Changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
 write_boot;
-## end boot install
-
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
+## end install
 
